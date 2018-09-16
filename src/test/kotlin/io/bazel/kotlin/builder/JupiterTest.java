949b6bb200c85049a687729603d89601a683b6c1
hsyed <h.a.syed@gmail.com>
2018-09-16 19:31:28 +0100

    checkpoint

---
+++ src/test/kotlin/io/bazel/kotlin/builder/JupiterTest.java
@@ -0,0 +1,10 @@
+package io.bazel.kotlin.builder;
+
+import org.junit.jupiter.api.Test;
+
+public class JupiterTest {
+    @Test
+    void testSomething() {
+
+    }
+}
---
+++ src/test/kotlin/io/bazel/kotlin/builder/KotlinTest.kt
@@ -0,0 +1,10 @@
+package io.bazel.kotlin.builder
+
+import org.junit.jupiter.api.Test
+
+class KotlinTest {
+    @Test
+    fun aTest() {
+
+    }
+}
